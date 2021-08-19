1d397d85ed0c9c209c03c904760c830569f4dcce
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-08-19 20:08:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video  = "";
+status = "";
+function preload(){
+  video = createCapture("video.mp4");
+  video.hide();
+}
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,480,380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Object Detecting!";
+}
+function modelLoaded(){
+    console.log("modelLoaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+
